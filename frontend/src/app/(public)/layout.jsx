37321ac658e218a403b38d1ca01fb29dcd073261
chanchal7068 <chanchalchauhanjibrand556@gmail.com>
2026-02-10 11:48:03 +0530

    updated

---
+++ frontend/src/app/(public)/layout.jsx
@@ -0,0 +1,9 @@
+export default function PublicLayout({ children }) {
+  return (
+    <div>
+        <main>
+            {children}
+        </main>
+    </div>
+  )
+}
--- frontend/src/app/(public)/page.jsx
+++ frontend/src/app/(public)/page.jsx
@@ -1,5 +1,7 @@
 export default function HomePage({ children }) {
   return (
-    
+    <div>
+        <h1>This is home page.</h1>
+    </div>
   )
 }
